95f91a89136b80c2715f79544c0b9c4833c75a9e
Anurag Gulati <92107977+Anurag-Gulati-IIITD@users.noreply.github.com>
2022-11-08 22:44:02 +0530

    Add files via upload

--- CPU.py
+++ CPU.py
@@ -10,14 +10,44 @@ from WriteBack import WriteBack
 
 class CPU:
     
-    def __init__(self):
-        self.imem = InstructionMemory(size=2**30)
-        self.dmem = DataMemory(size=2**30)
+    def __init__(self, binary, dMemLatency, dMemSize, iMemLatency, iMemSize):
+
+        self.imem = InstructionMemory(access_delay=iMemLatency, size=iMemSize)
+        self.dmem = DataMemory(access_latency=dMemLatency, size=dMemSize)
         self.rf = RegisterFile()
         self.F = Fetch(self.imem)
         self.D = Decode(self.rf)
         self.X = Execute()
         self.M = Memory(self.dmem)
         self.W = WriteBack()
+        self.binary = binary
+        self.PC = 0
     
-    def 
+    def initialize(self):
+        self.imem.initialize(self.binary)
+
+
+    def simulate(self):
+        
+        for cycle in range(1,100):
+
+            # F -> D
+            f2d = self.F.fetchToDecode()
+            # D -> X
+            d2x = self.D.decodeToExecute()
+            # X -> M
+
+            # M -> W
+
+            # Fetch
+            self.F.fetch(self.PC)
+            # Decode
+            (opcode_type, func, rd, rs1, rs2, offset) = self.D.decode(f2d)
+
+            # Execute
+
+            # Memory
+
+            # WriteBack
+
+

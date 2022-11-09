95ec59f290782f47d1b2547958a6c82b2182f47c
Shivamiiitd <88881225+Shivamiiitd@users.noreply.github.com>
2022-11-09 22:36:21 +0530

    Update CPU.py

--- CPU.py
+++ CPU.py
@@ -8,8 +8,9 @@ from Execute import Execute
 from Memory import Memory
 from WriteBack import WriteBack
 
+
 class CPU:
-    
+
     def __init__(self, binary, dMemLatency, dMemSize, iMemLatency, iMemSize):
 
         self.imem = InstructionMemory(access_delay=iMemLatency, size=iMemSize)
@@ -23,49 +24,48 @@ class CPU:
         self.binary = binary
         self.PC = -1
         self.branchTaken = False
-    
+
     def initBinary(self):
         self.imem.initialize(self.binary)
 
     def simulate(self):
-        
-        for cycle in range(1,10):
-            
+
+        for cycle in range(1, 10):
+
             # pass on to next stages
-            f2d = self.F.fetchToDecode()    # F -> D
+            f2d = self.F.fetchToDecode()  # F -> D
             d2x = self.D.decodeToExecute()  # D -> X
             x2m = self.X.executeToMemory()  # X -> M
-            m2w = self.M.memoryToWriteback()    # M -> W 
+            m2w = self.M.memoryToWriteback()  # M -> W
+            self.rf = self.W.write_Back()
 
             self.branchTaken = x2m[-1]
-    
+
             if self.branchTaken:
                 self.PC = x2m[2]
                 self.branchTaken = False
             else:
                 self.PC = self.PC + 1
-            
-            # run the pipeline stages          
-            if self.PC < len(self.imem.dump()):  
+
+            # run the pipeline stages
+            if self.PC < len(self.imem.dump()):
                 self.F.fetch(self.PC)
             else:
                 self.F.instruction = ''
             self.D.decode(f2d)
             self.X.execute_compute(d2x)
-            self.M.memory_compute(x2m)  
-            self.rf = self.W.writeback_compute(m2w)
+            self.M.memory_compute(x2m)
+            self.W.writeback_compute(m2w)
 
             print('Cycle: {}'.format(cycle))
             print('Fetch: {}'.format(self.F.instruction))
             print('Decode: {}'.format(self.D.instruction))
-            #print('Execute: {}'.format(d2x))
-        
+            # print('Execute: {}'.format(d2x))
+
         print('RF dump from CPU')
         print(self.rf.dump())
 
-system = CPU('testfile.txt',0,512,0,512)
+
+system = CPU('testfile.txt', 0, 512, 0, 512)
 system.initBinary()
 system.simulate()
-            
-            
-

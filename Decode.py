4c7ddcb5978e0ebb8cc03599011857bc0c7ae40d
poojasharma2212 <56320354+poojasharma2212@users.noreply.github.com>
2022-11-10 02:28:17 +0530

    Update Decode.py

--- Decode.py
+++ Decode.py
@@ -1,16 +1,17 @@
 class Decode:
 
     def __init__(self):
-        self.opcode_map = {'0010011': 'I', '0110011': 'R', '0100011': 'S', '1100011': 'SB'}
+        self.opcode_map = {'0010011': 'I', '0110011': 'R', '0100011': 'S', '1100011': 'SB', '0000011' : 'L'}
         self.instr_map = {
-            'I': {'010': 'lw', '000': 'addi'},
+            'I': {'000': 'addi'},
             'R': {
                 '0000000000': 'add', '0100000000': 'sub',
                 '0000000001': 'sll', '0100000101': 'sra',
                 '0000000110': 'or', '0000000111': 'and'
             },
             'S': {'010': 'sw'},
-            'SB': {'000': 'beq'}
+            'SB': {'000': 'beq'},
+            'L': {'010': 'lw'}
         }
         self.opcode = ''
         self.opcode_type = ''
@@ -33,6 +34,11 @@ class Decode:
                 self.rd = int(self.instruction[-12 - 1:-7 - 1], 2)
                 self.rs1 = int(self.instruction[-20 - 1:-15 - 1], 2)
                 self.offset = self.instruction[-32 - 1:-20 - 1]
+            if self.opcode_type == 'L':
+                self.func = self.instr_map['L'][self.instruction[-15 - 1:-12 - 1]]
+                self.rd = int(self.instruction[-12 - 1:-7 - 1], 2)
+                self.rs1 = int(self.instruction[-20 - 1:-15 - 1], 2)
+                self.offset = self.instruction[-32 - 1:-20 - 1]    
             elif self.opcode_type == 'R':
                 self.func = self.instr_map['R'][self.instruction[-32 - 1:-25 - 1] + self.instruction[-15 - 1:-12 - 1]]
                 self.rd = int(self.instruction[-12 - 1:-7 - 1], 2)
